c2f5e5a3aa5d27f27ceebf131b17216edacc939d
hadasbenisty <hadas.benisty@gmail.com>
2022-11-10 09:48:48 +0200

    Update main.m

--- workshop_code/main.m
+++ workshop_code/main.m
@@ -11,70 +11,70 @@ x = x(:, start_ind:end_ind);
 t = t(start_ind:end_ind);
 behavior_traces = behavior_traces(:, start_ind:end_ind);
 fsample = 10; % sampling frequency 
-%% Task 1: visualize data
-% a. plot the anatomic parcellation map saved in '../data/atlas.mat'
+%% Task A: visualize data
+% 1. plot the anatomic parcellation map saved in '../data/atlas.mat'
 load(anatomic_map_file, 'anatomic_parcels');
-% b. plot the functional parcellation map stored as func_map
+% 2. plot the functional parcellation map stored as func_map
 
-% c. choose the functional parcel corresponding to anatomical visual  
+% 3. choose the functional parcel corresponding to anatomical visual  
 %    cortex (anatomical #1) and corresponding to the motor cortex (anatomical #23) 
 %    and plot their traces vs time
-% d. plot behavior traces vs. time
+% 4. plot behavior traces vs. time
 
 
 
-%% Task 2: modeling behavior using neuronal activity
+%% Task B: modeling behavior using neuronal activity
 Kfolds = 10; % folds for cross validation
-% a. Use the function 'train_test_regression' to train a linear regression
+% 1. Use the function 'train_test_regression' to train a linear regression
 %    for each behavior trace based on neuronal activity using 10-fold
 %    cross-validation. What is the R2? Is it "train R2"? or "Test R2"? What 
 %    is the difference? 
-% b. Plot each behavior trace along with the model's prediction (use hold
+% 2. Plot each behavior trace along with the model's prediction (use hold
 %    all to plot both on the same axis
-% c. Use the function diffmap_euc to reduce the dimension of activity based on
+% 3. Use the function diffmap_euc to reduce the dimension of activity based on
 %    diffusion map. Use knn-param = 100. 
 [phi_x, Lambda_x1] = diffmap_euc(x, 100);
-% d. Use the function plot_latent to plot the first 3 components of the 
+% 4. Use the function plot_latent to plot the first 3 components of the 
 %    Embedded activity as a scatter plot. Colors should be z-scored pupil
 %    values.
-% e. Use different values of knn-param and scatter plot the components 
-% f. Plot the eigenvalues you got for different values of knn-param 
-% g. Train a regression model for behavior based on the embedded activity
+% 5. Use different values of knn-param and scatter plot the components 
+% 6. Plot the eigenvalues you got for different values of knn-param 
+% 7. Train a regression model for behavior based on the embedded activity
 % using the first 10 components
-% h. Plot behavior traces and estimated traces vs. time
+% 8. Plot behavior traces and estimated traces vs. time
 
-%% Task 3: modeling behavior using neuronal connectivity (Euc. Distance)
-% a. Write a function that extracts Pearson's correlation (use 'corr') using 
+%% Task C: modeling behavior using neuronal connectivity (Euc. Distance)
+% 1. Write a function that extracts Pearson's correlation (use 'corr') using 
 %    a sliding window. Input - x, window size (samples) and window hop (samples)
 %    Outputs - tC     - connectivity values of parcels over parcels over time points. 
 %              t_win  - indices of centers of analysis windows
 %    Use this function to extract correlations using 3sec window size and 
 %    0.1sec window hop 
-% b. Use the function cmat2feat to reshape tC into a pairwise correlation
+% 2. Use the function cmat2feat to reshape tC into a pairwise correlation
 %    over time matrix
-% c. Reduce the dim of c(t) using diff map (as you did for x(t))
-% d. resampling behavior to match windowed c(t)
-% e. Train a regression model for behavior based on the embedded
+% 3. Reduce the dim of c(t) using diff map (as you did for x(t))
+% 4. resampling behavior to match windowed c(t)
+% 5. Train a regression model for behavior based on the embedded
 % correlations using the first 10 components
-% f. Use the function plot_latent to plot the first 3 components of the
+% 6. Use the function plot_latent to plot the first 3 components of the
 % embedded correlations as a scatter where colors are zscored pupil values
-% g. Plot behavior traces and estimated traces vs. time
+% 7. Plot behavior traces and estimated traces vs. time
 
-%% Task 5: modeling behavior using neuronal connectivity (R-Distance)
+%% Task D: modeling behavior using neuronal connectivity (R-Distance)
 
 % load pre-calculated R-mean
 load('../data/Rmean_animal1');
-% a. Use the function getDiffMap_corr to reduce the dimension of
+% 1. Use the function getDiffMap_corr to reduce the dimension of
 % correlations. To save some time, use the pre-calculated 'mRiemannianMean'
 % stored in '../data/Rmean_animal1' as input. Use knn-param=100.
-% b. Model (resampled) behavior using the new embedded correlations
-% c. plot latent dynamics of the first 3 components with colors by pupil
-% d. Plot behavior traces and estimated traces vs. time
+% 2. Model (resampled) behavior using the new embedded correlations
+% 3. plot latent dynamics of the first 3 components with colors by pupil
+% 4. Plot behavior traces and estimated traces vs. time
 
-%% Task 6
+%% Task E
 % Visualize overall modeling results
 
-%% Task 7 - Animal 2
+%% Task F - Animal 2
 % Clear resuls and repeat for animal 2
 clear;
 close all;
